a9c0bcf80df8feb6e339fd6d460cedc4c0c6df80
Adarsh kumar yadav <adarshak4796@gmail.com>
2026-04-05 11:03:34 +0530

    add auto admin creation

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -144,9 +144,3 @@ import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@gmail.com', 'admin123')
